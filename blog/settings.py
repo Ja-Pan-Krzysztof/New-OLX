7cfb963a2a089d0e4549e77ad38ea3dd85ad40f7
Ja_Pan_Krzysztof <project.django.inne@gmail.com>
2022-10-26 07:07:58 +0200

    Fast change

--- blog/settings.py
+++ blog/settings.py
@@ -22,7 +22,7 @@ django_on_heroku.settings(locals())
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-load_dotenv(BASE_DIR / '.env')
+load_dotenv(BASE_DIR / '.env') or 'django-insecure-inksrr6pjkw3lno5z2@0k0199+v=!(=7b_r@h#wwz9d4_#h9qi'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
