263751a338587b1386b5f01ef0cd6201299f1c10
Pan_Krzysztof <74184329+Ja-Pan-Krzysztof@users.noreply.github.com>
2022-10-31 14:01:47 +0100

    Update forms.py

--- base/forms.py
+++ base/forms.py
@@ -1,18 +1,18 @@
 from django.contrib.auth.models import User
-from django.forms import ModelForm
+from django import forms
 from .models import Offer
 
 
-class UserForm(ModelForm):
+class UserForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['username', 'email']
 
 
-class OfferForm(ModelForm):
+class OfferForm(forms.ModelForm):
+
+    image = forms.ImageField(required=False)
+
     class Meta:
         model = Offer
         fields = ['topic', 'category', 'description', 'image', 'price']
-
-
-
